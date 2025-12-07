6f63c1540276864fde167ab6f515eb9d0d1f6b5a
Felix Maier <stoppi.felix@hotmail.com>
2025-12-07 20:16:25 +0100

    init Avalonia

---
+++ AudioSpectrumPlayer.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AudioSpectrumPlayer.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ AudioSpectrumPlayer.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AudioSpectrumPlayer.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
